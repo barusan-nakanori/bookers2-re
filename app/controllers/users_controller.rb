4cf9020379652e8e1dfc22902fe65c4a64d61cd0
vagrant <nori.work.vtr250@gmail.com>
2020-06-23 11:58:35 +0000

    add user/edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,22 @@ before_action :authenticate_user!
   	@user = User.find(params[:id])
   end
 
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		flash[:notice] = "You have updated user successfully."
+  		redirect_to user_path(@user)
+  	else
+  		render 'edit'
+  	end
+  end
+
   def show
   	@user = User.find(params[:id])
   end
+
+	  private
+	  def user_params
+	  	params.require(:user).permit(:name,:introduction,:profile_image)
+	  end
 end
