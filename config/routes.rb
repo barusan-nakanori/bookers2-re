27260c2873a47dd6acada3fa2e2c57a8377efc56
vagrant <nori.work.vtr250@gmail.com>
2020-06-23 10:40:36 +0000

    install refile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 	devise_for :users
   resources :users, only: [:show,:index,:edit,:update]
   root 'homes#top'
-  get '/about', to: 'homes#about'
+  get 'home/about', to: 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
