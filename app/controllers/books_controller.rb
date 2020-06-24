773b3b940aa80da00877d3ddbda02a3f7e937c65
vagrant <nori.work.vtr250@gmail.com>
2020-06-24 03:17:33 +0000

    add book/new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,26 @@
 class BooksController < ApplicationController
   def index
   	@books = Book.all
+  	@book = Book.new
   end
 
   def show
   end
 
+  def create
+  	@book = Book.new(params_book)
+  	if @book.save
+  		redirect_to book_path(@book)
+  	else
+  		render 'index'
+  	end
+  end
+
   def edit
   end
+
+	  private
+	  def params_book
+	  	params.require(:book).permit(:title,:body)
+	  end
 end
