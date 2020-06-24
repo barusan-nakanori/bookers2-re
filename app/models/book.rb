226a21c1ed39d0b01345c3451625fe844449386e
vagrant <nori.work.vtr250@gmail.com>
2020-06-24 02:52:49 +0000

    add book/index

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
 	validates :title,presence: true
-	validates :text, presence: true, length: {maxmum: 200}
+	validates :text, presence: true, length: {maximum: 200}
 	belongs_to :user
 end
