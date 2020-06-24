846ffced3bcd22409a793519684ffacf5dc6a71d
vagrant <nori.work.vtr250@gmail.com>
2020-06-24 02:31:56 +0000

    add book/controller,model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	devise_for :users
   resources :users, only: [:show,:index,:edit,:update]
+  resources :books, only: [:create,:show,:index,:edit,:update,:destroy]
   root 'homes#top'
   get 'home/about', to: 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
