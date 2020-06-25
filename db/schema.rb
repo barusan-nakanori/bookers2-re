9c25c321c0a2c4d6c99a50e6a0493f352aa47c8c
vagrant <nori.work.vtr250@gmail.com>
2020-06-25 03:25:24 +0000

    add favorite-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_022019) do
+ActiveRecord::Schema.define(version: 2020_06_25_030432) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.string "user_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_favorites_on_book_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
